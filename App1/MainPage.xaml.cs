dd9f52b836e26f3a5c0ad22ead2348963e984ff0
Michael Kreich <jaykay2k@hotmail.com>
2018-06-30 11:02:06 +0200

    initial commit

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, IViewFor<MainViewModel>
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+	        
+	        ViewModel = new MainViewModel();
+
+	        this.Bind(ViewModel, m => m.DummyText, v => v.TheTextBox.Text);
+	        this.OneWayBind(ViewModel, m => m.DummyText, v => v.TheTextBlock.Text);
+
+        }
+
+	    object IViewFor.ViewModel
+	    {
+		    get => ViewModel;
+		    set => ViewModel = (MainViewModel) value;
+	    }
+
+	    public MainViewModel ViewModel { get; set; }
+    }
+
+	public class MainViewModel : ReactiveObject
+	{
+		[Reactive]
+		public string DummyText { get; set; } = "";
+	}
+}
---
+++ UnitTestProject2/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using ReactiveUI;
+using Xunit;
+
+namespace UnitTestProject2
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void TestMethod1()
+		{
+			true.Should().BeTrue();
+			var reactiveList = new ReactiveList<string>();
+			var mainThreadScheduler = RxApp.MainThreadScheduler;
+		}
+	}
+}
